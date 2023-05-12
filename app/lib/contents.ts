7a939e084fa556b322bb81fded7beafc832f9710
I Komang Sena <i.buwana@btpn.com>
2023-05-13 00:49:35 +0700

    add data

---
+++ app/lib/contents.ts
@@ -0,0 +1,72 @@
+const contents = {
+    title: "PT. KONSULTAN PRODUKTIVITAS INDONESIA",
+    subtitle: "Indonesian Productivity Training and Consultancy",
+    address: "Learnotel Hotel & Convention\nJl. Cikopo Selatan No. 50 RT.001 RW. 04, Ds. Gadog\nKec. Megamendung, Kab. Bogor.\nKode Pos 16750",
+    phone: "081285924033",
+    email: "ptkpi@gmail.com",
+    projects: [
+        { 
+            title: "Productivity Training",
+            description: "PELATIHAN PRODUKTIVITAS (PENERAPAN ALAT/TEKNIK/METODE PENINGKATAN PRODUKTIVITAS",
+            policies: [
+                "Balanced Scorecard",
+                "Business Excellence Framework",
+                "Corporate social responsibility",
+                "Energy conservation/management",
+                "Environmental management system",
+                "Global Agricultural Practices (GAP)",
+                "ISO 9000 Quality Management System",
+                "National Quality Award",
+            ],
+            people: [
+                "Employee suggestion schemes",
+                "Lean (Toyota) Management System",
+                "OHSAS 18000",
+                "Social accountability (SA) 8000",
+                "Workplace cooperation",
+            ],
+            products: [
+                "3Rs: Redure, Reuse, Recycle",
+                "Customer Satisfaction Index",
+                "Eco-design",
+                "Hazard analysis and critical control points (HACCP)",
+                "Niche marketing",
+                "Quality management system",
+                "Supply chain management",
+            ],
+            processes: [
+                "Business process reengineering",
+                "Just-in-time production system",
+                "Preventive/productive maintenance",
+                "Six Sigma",
+            ]
+        },
+        {
+            title:"Productivity Certification",
+            
+        },
+        {title:"Productivity Consultant"},
+        {title:"Productivity Measurement"},
+        {title:"Productivity Research"},
+        {title:"Productivity Assistance"},
+    ],
+    vision: "Menjadi pusat pengembangan SDM dan perusahaan/organisasi/lembaga yang produktif dan berdaya saing",
+    missions: [
+        "Menyelenggarakan pelatihan peningkatan produktivitas.",
+        "Melaksanakan pengukuran produktivitas sebagai dasar perencanaan peningkatan produktivitas.",
+        "Memberikan konsultansi produktivitas kepada perusahaan/organisasi/lembaga dalam peningkatan produktivitas.",
+        "Melaksanakan pendampingan kepada Usaha Kecil Menengah dalam peningkatan produktivitas.",
+        "Melaksanakan kajian produktivitas.",
+        "Memfasilitasi sertifikasi kompetensi bidang produktivitas.",
+    ],
+    programs: [
+        "Improving productivity through training/ pelatihan produktivitas",
+        "Suistainability and responsibility enterprises through consultancy/ bimbingan konsultansi peningkatan produktivitas",
+        "Productivity measurement / pengukuran produktivitas",
+        "Research and development for productivity models/ penyiapan model  peningkatan produktivitas// Best Practice Benchmarking",
+        "Organizing for competencys sertification /memfasilitasi sertifikasi kompetensi",
+        "Produktivity Assistance for Small to Medium Enterprise /pendampingan produktivitas pada Usaha Kecil Menengah.",
+    ]
+}
+
+export default contents;

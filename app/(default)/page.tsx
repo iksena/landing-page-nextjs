2c51da59a5bf0c3dec8a0dc6b59a12aa2a21d8bb
I Komang Sena <iksena@aol.com>
2023-07-02 21:16:15 +0700

    add title

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Home - Open PRO',
-  description: 'Page description',
+  title: 'PT KPI - Konsultan Produktivitas Indonesia',
+  description: 'Website resmi Konsultan Produktivitas Indonesia',
 }
 
 import Hero from '@/components/hero'
